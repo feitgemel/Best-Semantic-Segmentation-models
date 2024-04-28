75de8c744b9ae8fae256498f235105fa15bf84a3
Eran Feit <feitgemel@gmail.com>
2024-04-28 18:39:09 +0300

    update comments

--- Segment-Anything/2-Segment-your-images-without-training/Segment-your-imaes-without-training.py
+++ Segment-Anything/2-Segment-your-images-without-training/Segment-your-imaes-without-training.py
@@ -36,6 +36,65 @@ sam = sam_model_registry[model_type](checkpoint=sam_checkpoint)
 sam.to(device=device)
 
 
+
+
+"""
+
+https://github.com/facebookresearch/segment-anything/blob/9e1eb9fdbc4bca4cd0d948b8ae7fe505d9f4ebc7/segment_anything/automatic_mask_generator.py#L35    
+
+        Arguments:
+          model (Sam): The SAM model to use for mask prediction.
+          
+          points_per_side (int or None): The number of points to be sampled
+            along one side of the image. The total number of points is
+            points_per_side**2. If None, 'point_grids' must provide explicit
+            point sampling.
+          
+          points_per_batch (int): Sets the number of points run simultaneously
+            by the model. Higher numbers may be faster but use more GPU memory.
+          
+          pred_iou_thresh (float): A filtering threshold in [0,1], using the
+            model's predicted mask quality.
+          
+          stability_score_thresh (float): A filtering threshold in [0,1], using
+            the stability of the mask under changes to the cutoff used to binarize
+            the model's mask predictions.
+          
+          stability_score_offset (float): The amount to shift the cutoff when
+            calculated the stability score.
+          
+          box_nms_thresh (float): The box IoU cutoff used by non-maximal
+            suppression to filter duplicate masks.
+          
+          crops_n_layers (int): If >0, mask prediction will be run again on
+            crops of the image. Sets the number of layers to run, where each
+            layer has 2**i_layer number of image crops.
+          
+          crops_nms_thresh (float): The box IoU cutoff used by non-maximal
+            suppression to filter duplicate masks between different crops.
+          
+          crop_overlap_ratio (float): Sets the degree to which crops overlap.
+            In the first crop layer, crops will overlap by this fraction of
+            the image length. Later layers with more crops scale down this overlap.
+          
+          crop_n_points_downscale_factor (int): The number of points-per-side
+            sampled in layer n is scaled down by crop_n_points_downscale_factor**n.
+          
+          point_grids (list(np.ndarray) or None): A list over explicit grids
+            of points used for sampling, normalized to [0,1]. The nth grid in the
+            list is used in the nth crop layer. Exclusive with points_per_side.
+          
+          min_mask_region_area (int): If >0, postprocessing will be applied
+            to remove disconnected regions and holes in masks with area smaller
+            than min_mask_region_area. Requires opencv.
+          
+          output_mode (str): The form masks are returned in. Can be 'binary_mask',
+            'uncompressed_rle', or 'coco_rle'. 'coco_rle' requires pycocotools.
+            For large resolutions, 'binary_mask' may consume large amounts of
+            memory.
+"""
+
+
 mask_generator_ = SamAutomaticMaskGenerator(
     model=sam,
     points_per_side=32,
@@ -48,6 +107,20 @@ mask_generator_ = SamAutomaticMaskGenerator(
 
 masks = mask_generator_.generate(image)
 print("Total mask discovered : " + str(len(masks)))
+
+
+"""
+Mask generation returns a list over masks, where each mask is a dictionary containing various data about the mask. These keys are:
+
+segmentation : the mask
+area : the area of the mask in pixels
+bbox : the boundary box of the mask in XYWH format
+predicted_iou : the model's own prediction for the quality of the mask
+point_coords : the sampled input point that generated this mask
+stability_score : an additional measure of mask quality
+crop_box : the crop of the image used to generate this mask in XYWH format
+
+"""
 
 
 # show the results :
